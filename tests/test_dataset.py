f8f514bc4bf1fbe14285b5b780bca9abb982b3c1
xiaoke@ivgsz_170 <xiaokehuang@foxmail.com>
2022-05-22 15:22:57 +0800

    add LLFF dataset

--- tests/test_dataset.py
+++ tests/test_dataset.py
@@ -1,18 +1,19 @@
+import os
+
 import torch
 
 from yanerf.dataset.blender_dataset import BlenderDataset
 from yanerf.dataset.builder import DATASETS
+from yanerf.dataset.llff_dataset import LLFFDataset
 from yanerf.runners.utils import create_loader, create_sampler, get_rank, get_world_size
 from yanerf.utils.config import Config
 from yanerf.utils.logging import get_logger
-
-import os
 
 os.makedirs("tests/tmp/", exist_ok=True)
 logger = get_logger(__name__, log_file="tests/tmp/log.log")
 
 
-def test_dataset():
+def test_blenderdataset():
     cfg = Config.fromfile("tests/configs/data/dataset.yml")
     cfg.dataset["debug"] = True
     print(cfg.pretty_text)
@@ -32,3 +33,49 @@ def test_dataset():
     for data in blob:
         assert data.dtype == torch.float32
         logger.info(f"{data.shape}, {data.dtype}, {data.device}")
+
+
+def test_llffdataset():
+    cfg = {
+        "type": "LLFFDataset",
+        "split": "train",
+        "base_dir": "data/nerf_llff/fern",
+    }
+    cfg["debug"] = True
+    print(cfg)
+    try:
+        dataset: LLFFDataset = DATASETS.build(cfg)
+    except FileNotFoundError:
+        return
+    dataset[0]
+    sampler = create_sampler(dataset, shuffle=True, rank=get_rank(), world_size=get_world_size())
+
+    batch_size = 3
+    num_workers = 2
+    dataloader = create_loader(
+        dataset=dataset, sampler=sampler, batch_size=batch_size, num_workers=num_workers, is_train=True, collate_fn=None
+    )
+
+    blob = next(iter(dataloader))
+    for data in blob:
+        assert data.dtype == torch.float32
+        logger.info(f"{data.shape}, {data.dtype}, {data.device}")
+
+    cfg["split"] = "val"
+    try:
+        dataset: LLFFDataset = DATASETS.build(cfg)
+    except FileNotFoundError:
+        return
+    sampler = create_sampler(dataset, shuffle=True, rank=get_rank(), world_size=get_world_size())
+
+    batch_size = 1
+    num_workers = 2
+    dataloader = create_loader(
+        dataset=dataset, sampler=sampler, batch_size=batch_size, num_workers=num_workers, is_train=True, collate_fn=None
+    )
+
+    blob = next(iter(dataloader))
+    for data in blob:
+        assert data.dtype == torch.float32
+        logger.info(f"{data.shape}, {data.dtype}, {data.device}")
+    # focal is wrong
--- yanerf/dataset/llff_dataset.py
+++ yanerf/dataset/llff_dataset.py
@@ -1,8 +1,7 @@
-import json
-from pathlib import Path
+import os
 from typing import Callable, NamedTuple, Tuple
 
-import cv2  # type: ignore[import]
+import imageio
 import numpy as np
 import torch
 from torch.utils.data import Dataset
@@ -15,61 +14,369 @@ from .utils import load_image
 logger = get_logger(__name__)
 
 
-class BlenderDatasetWrapper(NamedTuple):
+class LLFFDatasetWrapper(NamedTuple):
     poses: torch.Tensor
     focal_lengths: torch.Tensor
     image_rgb: torch.Tensor
+    min_depth: torch.Tensor
+    max_depth: torch.Tensor
 
 
 @DATASETS.register_module()
 class LLFFDataset(Dataset):
-    data_wrapper: Callable = BlenderDatasetWrapper
+    data_wrapper: Callable = LLFFDatasetWrapper
 
-    def __init__(self, base_dir, split, scale_down=1, debug=False):
+    def __init__(
+        self,
+        base_dir,
+        split,
+        test_skip=8,
+        factor=8,
+        recenter=True,
+        bd_factor=0.75,
+        spherify=False,
+        path_zflat=False,
+        debug=False,
+    ):
         if split not in ["train", "val", "test"]:
             raise ValueError(f"Invalid split: {split}.")
 
-        self.base_dir = Path(base_dir)
-        self.split = split
-        with open(self.base_dir / f"transforms_{split}.json", "r") as fp:
-            meta = json.load(fp)
-        self.frames = meta["frames"]
-        camera_angle_x = float(meta["camera_angle_x"])
+        poses, bds, imgfiles = LLFFDataset._load_data(base_dir, factor=factor)
 
-        img_path = self.base_dir / f"{self.frames[0]['file_path']}.png"
-        img = load_image(img_path)
-        H, W = img.shape[:2]
-        focal = 0.5 * W / np.tan(0.5 * camera_angle_x)
+        # Correct rotation matrix ordering and move variable dim to axis 0
+        poses = np.concatenate([poses[:, 1:2, :], -poses[:, 0:1, :], poses[:, 2:, :]], 1)
+        poses = np.moveaxis(poses, -1, 0).astype(np.float32)
+        bds = np.moveaxis(bds, -1, 0).astype(np.float32)
 
-        if debug:
-            scale_down = 32
-            logger.info(f"[DEBUG] scale_down from {H}x{W} to {H//scale_down}x{W//scale_down}")
+        # Rescale if bd_factor is provided
+        sc = 1.0 if bd_factor is None else 1.0 / (bds.min() * bd_factor)
+        poses[:, :3, 3] *= sc
+        bds *= sc
 
-        if scale_down < 0 or not isinstance(scale_down, (float, int)):
-            raise TypeError(f"Invalid type scale_down: {type(scale_down)}.")
-        self.H = H // scale_down
-        self.W = W // scale_down
-        self.focal = focal / scale_down
-        self.scale_down = scale_down
+        if recenter:
+            poses = recenter_poses(poses)
+
+        if spherify:
+            poses, render_poses, bds = spherify_poses(poses, bds)
+
+        else:
+
+            c2w = poses_avg(poses)
+            logger.info(f"recentered: {c2w.shape}")
+            logger.info(f"{c2w[:3, :4]}")
+
+            # Get spiral
+            # Get average pose
+            up = normalize(poses[:, :3, 1].sum(0))
+
+            # Find a reasonable "focus depth" for this dataset
+            close_depth, inf_depth = bds.min() * 0.9, bds.max() * 5.0
+            dt = 0.75
+            mean_dz = 1.0 / (((1.0 - dt) / close_depth + dt / inf_depth))
+            focal = mean_dz
+
+            # Get radii for spiral path
+            shrink_factor = 0.8
+            zdelta = close_depth * 0.2
+            tt = poses[:, :3, 3]  # ptstocam(poses[:3,3,:].T, c2w).T
+            rads = np.percentile(np.abs(tt), 90, 0)
+            c2w_path = c2w
+            N_views = 120
+            N_rots = 2
+            if path_zflat:
+                # zloc = np.percentile(tt, 10, 0)[2]
+                zloc = -close_depth * 0.1
+                c2w_path[:3, 3] = c2w_path[:3, 3] + zloc * c2w_path[:3, 2]
+                rads[2] = 0.0
+                N_rots = 1
+                N_views /= 2
+
+            # Generate poses for spiral path
+            render_poses = render_path_spiral(c2w_path, up, rads, focal, zdelta, zrate=0.5, rots=N_rots, N=N_views)
+
+        render_poses = np.array(render_poses).astype(np.float32)
+
+        c2w = poses_avg(poses)
+        logger.info(f"Data:\n{poses.shape}, {bds.shape}")
+
+        if test_skip > 0:
+            i_test = np.array([i for i in range(0, len(imgfiles), test_skip)])
+        else:
+            dists = np.sum(np.square(c2w[:3, 3] - poses[:, :3, 3]), -1)
+            i_test = np.argmin(dists)
+            i_test = np.array([i_test])
+        logger.info(f"HOLDOUT view is/are:\n{i_test}")
+
+        poses = poses.astype(np.float32)
+        imgfiles = np.array(imgfiles)
+
+        if split in ("val", "test"):
+            poses = poses[i_test]
+            imgfiles = imgfiles[i_test]
+            bds = bds[i_test]
+        else:
+            i_train = np.array([i for i in range(len(imgfiles)) if i not in i_test])
+            poses = poses[i_train]
+            imgfiles = imgfiles[i_train]
+            bds = bds[i_train]
+
+        self.poses = poses
+        self.imgfiles = imgfiles
+        self.bds = bds
+
+        self.render_poses = render_poses
 
         calib_mat = np.eye(4).astype(np.float32)
         calib_mat[1, 1] = calib_mat[2, 2] = -1.0
         self.calib_mat = calib_mat
 
     def __getitem__(self, index: int) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
-        file_path = self.frames[index]["file_path"]
+        file_path = self.imgfiles[index]
 
-        pose = np.array(self.frames[index]["transform_matrix"]).astype(np.float32)
+        pose = self.poses[index].astype(np.float32)
+        h, w, focal = pose[:, -1]
+        pose = pose[:, :4]
+        min_depth, max_depth = self.bds[index].astype(np.float32)
+
         # FIX: for both camera and world coordiantes,
         # we use right-hand system, but the z-axis for the camera space is pointed inward the screen;
         # that of the world space is outward (for NeRF synthetics dataset)
         pose = pose @ self.calib_mat
 
-        normalized_img = load_image(self.base_dir / f"{file_path}.png")
-        if self.scale_down != 1:
-            normalized_img = cv2.resize(normalized_img, dsize=(self.H, self.W), interpolation=cv2.INTER_LINEAR)
+        normalized_img = load_image(file_path)
 
-        return torch.from_numpy(pose), torch.FloatTensor([self.focal]), torch.from_numpy(normalized_img)
+        return (
+            torch.from_numpy(pose),
+            torch.FloatTensor([focal]),
+            torch.from_numpy(normalized_img),
+            torch.FloatTensor([min_depth]),
+            torch.FloatTensor([max_depth]),
+        )
 
     def __len__(self):
-        return len(self.frames)
+        return len(self.imgfiles)
+
+    @staticmethod
+    def _load_data(basedir, factor=None, width=None, height=None, load_imgs=True):
+        poses_arr = np.load(os.path.join(basedir, "poses_bounds.npy"))
+        poses = poses_arr[:, :-2].reshape([-1, 3, 5]).transpose([1, 2, 0])
+        bds = poses_arr[:, -2:].transpose([1, 0])
+
+        img0 = [
+            os.path.join(basedir, "images", f)
+            for f in sorted(os.listdir(os.path.join(basedir, "images")))
+            if f.endswith("JPG") or f.endswith("jpg") or f.endswith("png")
+        ][0]
+        sh = imageio.imread(img0).shape
+
+        sfx = ""
+
+        if factor is not None:
+            sfx = f"_{factor}"
+            LLFFDataset._minify(basedir, factors=[factor])
+            factor = factor
+        elif height is not None:
+            factor = sh[0] / float(height)
+            width = int(sh[1] / factor)
+            LLFFDataset._minify(basedir, resolutions=[[height, width]])
+            sfx = f"_{width}x{height}"
+        elif width is not None:
+            factor = sh[1] / float(width)
+            height = int(sh[0] / factor)
+            LLFFDataset._minify(basedir, resolutions=[[height, width]])
+            sfx = f"_{width}x{height}"
+        else:
+            factor = 1
+
+        imgdir = os.path.join(basedir, "images" + sfx)
+        if not os.path.exists(imgdir):
+            logger.info(f"{fimgdir} does not exist, returning")
+            return
+
+        imgfiles = [
+            os.path.join(imgdir, f)
+            for f in sorted(os.listdir(imgdir))
+            if f.endswith("JPG") or f.endswith("jpg") or f.endswith("png")
+        ]
+        if poses.shape[-1] != len(imgfiles):
+            logger.info(f"Mismatch between imgs {len(imgfiles)} and poses {poses.shape[-1]} !!!!")
+            return
+
+        sh = imageio.imread(imgfiles[0]).shape
+        poses[:2, 4, :] = np.array(sh[:2]).reshape([2, 1])
+        poses[2, 4, :] = poses[2, 4, :] * 1.0 / factor
+
+        return poses, bds, imgfiles
+
+    @staticmethod
+    def _minify(basedir, factors=[], resolutions=[]):
+        needtoload = False
+        for r in factors:
+            imgdir = os.path.join(basedir, f"images_{r}")
+            if not os.path.exists(imgdir):
+                needtoload = True
+        for r in resolutions:
+            imgdir = os.path.join(basedir, f"images_{r[1]}x{r[0]}")
+            if not os.path.exists(imgdir):
+                needtoload = True
+        if not needtoload:
+            return
+
+        from shutil import copy
+        from subprocess import check_output
+
+        imgdir = os.path.join(basedir, "images")
+        imgs = [os.path.join(imgdir, f) for f in sorted(os.listdir(imgdir))]
+        imgs = [f for f in imgs if any([f.endswith(ex) for ex in ["JPG", "jpg", "png", "jpeg", "PNG"]])]
+        imgdir_orig = imgdir
+
+        wd = os.getcwd()
+
+        for r in factors + resolutions:
+            if isinstance(r, int):
+                name = f"images_{r}"
+                resizearg = f"{100.0 / r}%"
+            else:
+                name = f"images_{r[1]}x{r[0]}"
+                resizearg = f"{r[1]}x{r[0]}"
+            imgdir = os.path.join(basedir, name)
+            if os.path.exists(imgdir):
+                continue
+
+            logger.info(f"Minifying, {r}, {basedir}")
+
+            os.makedirs(imgdir)
+            check_output(f"cp {imgdir_orig}/* {imgdir}", shell=True)
+
+            ext = imgs[0].split(".")[-1]
+            args = " ".join(["mogrify", "-resize", resizearg, "-format", "png", f"*.{ext}"])
+            logger.info(f"cmd: {args}")
+            os.chdir(imgdir)
+            check_output(args, shell=True)
+            os.chdir(wd)
+
+            if ext != "png":
+                check_output(f"rm {imgdir}/*.{ext}", shell=True)
+                logger.info("Removed duplicates")
+            logger.info("Done")
+
+
+def normalize(x):
+    return x / np.linalg.norm(x)
+
+
+def viewmatrix(z, up, pos):
+    vec2 = normalize(z)
+    vec1_avg = up
+    vec0 = normalize(np.cross(vec1_avg, vec2))
+    vec1 = normalize(np.cross(vec2, vec0))
+    m = np.stack([vec0, vec1, vec2, pos], 1)
+    return m
+
+
+def ptstocam(pts, c2w):
+    tt = np.matmul(c2w[:3, :3].T, (pts - c2w[:3, 3])[..., np.newaxis])[..., 0]
+    return tt
+
+
+def poses_avg(poses):
+
+    hwf = poses[0, :3, -1:]
+
+    center = poses[:, :3, 3].mean(0)
+    vec2 = normalize(poses[:, :3, 2].sum(0))
+    up = poses[:, :3, 1].sum(0)
+    c2w = np.concatenate([viewmatrix(vec2, up, center), hwf], 1)
+
+    return c2w
+
+
+def render_path_spiral(c2w, up, rads, focal, zdelta, zrate, rots, N):
+    render_poses = []
+    rads = np.array(list(rads) + [1.0])
+    hwf = c2w[:, 4:5]
+
+    for theta in np.linspace(0.0, 2.0 * np.pi * rots, N + 1)[:-1]:
+        c = np.dot(c2w[:3, :4], np.array([np.cos(theta), -np.sin(theta), -np.sin(theta * zrate), 1.0]) * rads)
+        z = normalize(c - np.dot(c2w[:3, :4], np.array([0, 0, -focal, 1.0])))
+        render_poses.append(np.concatenate([viewmatrix(z, up, c), hwf], 1))
+    return render_poses
+
+
+def recenter_poses(poses):
+
+    poses_ = poses + 0
+    bottom = np.reshape([0, 0, 0, 1.0], [1, 4])
+    c2w = poses_avg(poses)
+    c2w = np.concatenate([c2w[:3, :4], bottom], -2)
+    bottom = np.tile(np.reshape(bottom, [1, 1, 4]), [poses.shape[0], 1, 1])
+    poses = np.concatenate([poses[:, :3, :4], bottom], -2)
+
+    poses = np.linalg.inv(c2w) @ poses
+    poses_[:, :3, :4] = poses[:, :3, :4]
+    poses = poses_
+    return poses
+
+
+#####################
+
+
+def spherify_poses(poses, bds):
+
+    p34_to_44 = lambda p: np.concatenate([p, np.tile(np.reshape(np.eye(4)[-1, :], [1, 1, 4]), [p.shape[0], 1, 1])], 1)
+
+    rays_d = poses[:, :3, 2:3]
+    rays_o = poses[:, :3, 3:4]
+
+    def min_line_dist(rays_o, rays_d):
+        A_i = np.eye(3) - rays_d * np.transpose(rays_d, [0, 2, 1])
+        b_i = -A_i @ rays_o
+        pt_mindist = np.squeeze(-np.linalg.inv((np.transpose(A_i, [0, 2, 1]) @ A_i).mean(0)) @ (b_i).mean(0))
+        return pt_mindist
+
+    pt_mindist = min_line_dist(rays_o, rays_d)
+
+    center = pt_mindist
+    up = (poses[:, :3, 3] - center).mean(0)
+
+    vec0 = normalize(up)
+    vec1 = normalize(np.cross([0.1, 0.2, 0.3], vec0))
+    vec2 = normalize(np.cross(vec0, vec1))
+    pos = center
+    c2w = np.stack([vec1, vec2, vec0, pos], 1)
+
+    poses_reset = np.linalg.inv(p34_to_44(c2w[None])) @ p34_to_44(poses[:, :3, :4])
+
+    rad = np.sqrt(np.mean(np.sum(np.square(poses_reset[:, :3, 3]), -1)))
+
+    sc = 1.0 / rad
+    poses_reset[:, :3, 3] *= sc
+    bds *= sc
+    rad *= sc
+
+    centroid = np.mean(poses_reset[:, :3, 3], 0)
+    zh = centroid[2]
+    radcircle = np.sqrt(rad**2 - zh**2)
+    new_poses = []
+
+    for th in np.linspace(0.0, 2.0 * np.pi, 120):
+
+        camorigin = np.array([radcircle * np.cos(th), radcircle * np.sin(th), zh])
+        up = np.array([0, 0, -1.0])
+
+        vec2 = normalize(camorigin)
+        vec0 = normalize(np.cross(vec2, up))
+        vec1 = normalize(np.cross(vec2, vec0))
+        pos = camorigin
+        p = np.stack([vec0, vec1, vec2, pos], 1)
+
+        new_poses.append(p)
+
+    new_poses = np.stack(new_poses, 0)
+
+    new_poses = np.concatenate([new_poses, np.broadcast_to(poses[0, :3, -1:], new_poses[:, :3, -1:].shape)], -1)
+    poses_reset = np.concatenate(
+        [poses_reset[:, :3, :4], np.broadcast_to(poses[0, :3, -1:], poses_reset[:, :3, -1:].shape)], -1
+    )
+
+    return poses_reset, new_poses, bds
